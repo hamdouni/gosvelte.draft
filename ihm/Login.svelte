46be6c9e68f922c3686640b9d8c706ba5cc37e46
Brahim Hamdouni <brahim@hamdouni.com>
2021-05-25 17:27:13 +0200

    Ajaxify login coté client

--- ihm/Login.svelte
+++ ihm/Login.svelte
@@ -1,5 +1,17 @@
-<form action="/login" method="post">
-    <input type="text" name="username" placeholder="Utilisateur">
-    <input type="password" name="password" placeholder="Mot de passe">
+<script>
+let username;
+let password;
+async function login() {
+    let response = await fetch("/login",{
+        method: "POST",
+        body: 'username='+username+'&password='+password,
+        headers: {"Content-Type": "application/x-www-form-urlencoded"}
+    });
+}
+</script>
+
+<form on:submit|preventDefault={login} action="/login" method="post">
+    <input type="text" name="username" placeholder="Utilisateur" bind:value={username}>
+    <input type="password" name="password" placeholder="Mot de passe" bind:value={password}>
     <button type="submit">Connecter</button>
 </form>
