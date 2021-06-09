ff2d316f5f4ec58af557d3eb7b9ebb2a9eb93998
Brahim Hamdouni <brahim@hamdouni.com>
2021-06-09 19:33:04 +0200

    Restructure la partie client et intègre BulmaCSS

--- ihm/Login.svelte
+++ ihm/Login.svelte
@@ -1,9 +1,9 @@
 <script>
-import { createEventDispatcher } from 'svelte';
-const dispatch = createEventDispatcher();
+export let connectedStatus = '';
 
 let username;
 let password;
+let err;
 
 async function login() {
     let response = await fetch("/login",{
@@ -12,15 +12,52 @@ async function login() {
         headers: {"Content-Type": "application/x-www-form-urlencoded"}
     });
     if(response.ok) {
-        dispatch('connected');
+        connectedStatus = true;
     } else {
-        dispatch('notconnected')
+        connectedStatus = false;
+        err = "Echec de la connexion. Veuillez vérifier votre identifiant et votre mot de passe. Si le problème persiste, merci de contacter l'administrateur.";
     }
 }
 </script>
 
-<form on:submit|preventDefault={login} action="/login" method="post">
-    <input type="text" name="username" placeholder="Utilisateur" bind:value={username}>
-    <input type="password" name="password" placeholder="Mot de passe" bind:value={password}>
-    <button type="submit">Connecter</button>
-</form>
+<login class="card">
+    <!-- svelte-ignore a11y-autofocus-->
+    <form on:submit|preventDefault={login} action="/login" method="post" class="card-content">
+        <div class="field">
+            <label for="username" class="label">Identifiant</label>
+            <div class="control has-icons-left has-icons-right">
+                <input class="input" type="text" name="username" bind:value={username} autofocus>
+                <span class="icon is-small is-left">
+                    <i class="fas fa-user"></i>
+                </span>
+            </div>
+        </div>
+        <div class="field">
+            <label for="password" class="label">Mot de passe</label>
+            <div class="control has-icons-left has-icons-right">
+                <input class="input" type="password" name="password" bind:value={password}>
+                <span class="icon is-small is-left">
+                    <i class="fas fa-unlock-alt"></i>
+                </span>
+            </div>
+        </div>
+        <div class="buttons">
+            <button class="button is-primary is-large" on:click|preventDefault={login}>
+                <strong>
+                    Connecter
+                </strong>
+            </button>
+        </div>
+        <div>
+            {#if err}
+            <article class="message is-danger">
+                <div class="message-body">
+                    <strong>
+                        {err}
+                    </strong>
+                </div>
+            </article>
+            {/if}
+        </div>
+    </form>
+</login>
---
+++ ihm/functions/Min.svelte
@@ -0,0 +1,22 @@
+<script>
+    import * as net from "../lib/network.js";
+    let nom;
+    let resultat;
+
+    async function min() {
+        let res = await net.callMin(nom);
+        if (res != null) resultat = res;
+    }
+</script>
+
+<div>
+    <input bind:value="{nom}" type="text" placeholder="Entrer le mot ou la phrase à mettre en minuscule..."
+        class="input">
+    <button on:click="{min}">
+        Minuscule
+    </button>
+    <hr>
+    {#if resultat}
+    {resultat}
+    {/if}
+</div>
