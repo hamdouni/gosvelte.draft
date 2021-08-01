42f79d2bb13651535621d515a2659e57c9356216
Brahim Hamdouni <brahim@hamdouni.com>
2021-08-01 16:13:49 +0200

    Comply to static check analyse

--- api/web/web_srv_hello.go
+++ api/web/web_srv_hello.go
@@ -6,7 +6,10 @@ import "net/http"
 	Hello implémente la fonction en charge de l'url "/hello". Pour être considérée comme une HandleFunc, elle doit obligatoirement accepter en paramètre une http.ResponseWriter et un pointeur sur une http.Request
 */
 func (web *WEB) Hello(w http.ResponseWriter, r *http.Request) {
-	r.ParseForm()
+	if err := r.ParseForm(); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
 	nom := r.Form.Get("nom")
 	message := web.biz.Bonjour(nom)
 	respondJSON(w, http.StatusOK, message)
