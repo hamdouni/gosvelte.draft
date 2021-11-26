3b4c3d84cb4e73b2876e41394da62f38345f9cf2
Brahim Hamdouni <brahim@hamdouni.com>
2021-11-26 18:54:11 +0100

    refactor: move server in api, make it work w/ compose

--- api/server/server.go
+++ api/server/server.go
@@ -19,8 +19,9 @@ func main() {
 
 func run(args []string) error {
 	flags := flag.NewFlagSet(args[0], flag.ExitOnError)
-	host := flags.String("host", "127.0.0.1", "host name to listen on")
+	host := flags.String("host", "0.0.0.0", "host name to listen on")
 	port := flags.Int("port", 8000, "specify port to listen on")
+	static := flags.String("static", "./client/static", "folder to html css js static files")
 	help := flags.Bool("help", false, "show command usage")
 	if err := flags.Parse(args[1:]); err != nil {
 		return err
@@ -43,7 +44,7 @@ func run(args []string) error {
 	storage.AddUser(user.Username, user.Password)
 
 	var api api.API
-	if err := api.Init(&storage, "./client/static"); err != nil {
+	if err := api.Init(&storage, *static); err != nil {
 		return fmt.Errorf("impossible d'initialiser l'api: %v", err)
 	}
 
