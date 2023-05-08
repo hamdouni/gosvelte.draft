22967904b1e65d33064ad67b287df5f586e8a7fd
Brahim Hamdouni <brahim@hamdouni.com>
2023-05-08 19:26:09 +0200

    make private server struct

--- api/server.go
+++ api/server.go
@@ -5,16 +5,16 @@ import (
 	"net/http"
 )
 
-// Server sert des pages dynamiques et statiques depuis le dossier RootDir en
+// server sert des pages dynamiques et statiques depuis le dossier RootDir en
 // écoutant sur Address de la forme `host:port` par exemple `acme.com:80`.
-type Server struct {
+type server struct {
 	RootDir string
 	Address string
 }
 
 // New retourne un serveur à partir d'un dossier, d'un host et d'un port
-func New(dir, host string, port int) Server {
-	return Server{
+func New(dir, host string, port int) server {
+	return server{
 		RootDir: dir,
 		Address: fmt.Sprintf("%s:%d", host, port),
 	}
@@ -24,7 +24,7 @@ func New(dir, host string, port int) Server {
 // Pour les fichiers statiques (html, js, images, ...), la librairie standard
 // propose une fonction FileServer. Pour les traitements dynamiques, on déclare
 // les routes en gérant le fait qu'elles doivent être authentifiées ou non.
-func (s Server) Run() error {
+func (s server) Run() error {
 	fs := http.FileServer(http.Dir(s.RootDir))
 	http.Handle("/", fs)
 
