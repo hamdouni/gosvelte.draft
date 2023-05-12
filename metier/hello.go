1fdfcaa2286985e5ac8073cc0b6346aa5fd5e52d
Brahim Hamdouni <brahim@hamdouni.com>
2023-05-12 15:40:09 +0200

    francise le message de bonjour

--- metier/hello.go
+++ metier/hello.go
@@ -2,6 +2,6 @@ package metier
 
 // Hello retourne bonjour et le nom passé en paramètre.
 func Hello(s string) string {
-	res := "Bonjour " + s + " depuis le business !"
+	res := "Bonjour " + s + " depuis le métier !"
 	return res
 }
--- metier/hello_test.go
+++ metier/hello_test.go
@@ -7,7 +7,7 @@ import (
 
 func TestBonjour(t *testing.T) {
 	given := "Jean"
-	wait := "Bonjour Jean depuis le business !"
+	wait := "Bonjour Jean depuis le métier !"
 	got := metier.Hello(given)
 	if got != wait {
 		t.Fatalf("Waiting %v but got %v", wait, got)
